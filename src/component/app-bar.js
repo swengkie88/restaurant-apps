6e86c1c8ee790e1de119acf371ec401d42d81ad1
swengkie88 <angkiprana1987@gmail.com>
2022-10-06 07:17:13 +0700

    Fixing accesbility issues

--- src/component/app-bar.js
+++ src/component/app-bar.js
@@ -8,7 +8,7 @@ class AppBar extends HTMLElement{
             <div class="container">
                 <div class='header-content'>
                     <a href="/" class="logo">O<span class="highlight-text">maem</span></a>
-                    <a id="hamburger">☰</a>
+                    <a id="hamburger" href="#">☰</a>
                 </div>
                 <nav id="navbar"> 
                         <ul> 
